e770d03fc4c65ea4a033cb4f461de5445062edef
Santhosh-N <59107079+Santhosh-N@users.noreply.github.com>
2024-11-05 21:32:11 +0530

    Add files via upload

---
+++ Runtime/TestOne.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Santhosh.Package
+{
+    public class TestOne : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Santhanam");
+        }
+
+    }
+}
