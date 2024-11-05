d1687940dbc0893eb4414586e5285447f6f621b6
santhoshleagueX <santhoshnagarajan321@gmail.com>
2024-11-05 21:57:52 +0530

    .

---
+++ Runtime/TestTwo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Santhosh.Package
+{
+    public class TestTwo : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("2");
+        }
+
+    }
+}
